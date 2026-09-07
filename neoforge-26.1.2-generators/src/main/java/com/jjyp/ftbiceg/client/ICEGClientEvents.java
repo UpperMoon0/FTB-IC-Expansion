851344abfbccbb2b7fc44c6a10e36102f8a90b63
Sunday Vo <60876500+UpperMoon0@users.noreply.github.com>
2026-09-08 02:24:18 +0700

    Fix 26.1.2 client event subscriber

--- neoforge-26.1.2-generators/src/main/java/com/jjyp/ftbiceg/client/ICEGClientEvents.java
+++ neoforge-26.1.2-generators/src/main/java/com/jjyp/ftbiceg/client/ICEGClientEvents.java
@@ -9,7 +9,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-@EventBusSubscriber(modid = FTBICEG.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = FTBICEG.MODID, value = Dist.CLIENT)
 public final class ICEGClientEvents {
     private ICEGClientEvents() {
     }
